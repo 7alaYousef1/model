1cc9a6a2c2367401192ed7de709440dea8ad5b4e
unknown <shathanadirr22f@gmail.com>
2023-07-06 22:03:16 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,28 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+// كلاس route بيشتغل عادي بدون ما اعمل use  لانه كلاسات معرفة glouble وهو منهم
+//بقدر اشوف الكلاسات المعرفة عن طريق ctrl  واضغط على الملف الموجود في routes app اخر واحد
+
+////كل يلي بلارفيل بيكونوا static
+// aninoums function لحالها بتكون بدون استدعاء زي يلي بتكون في الroute
+
+//namespace لتعريف الكلاس
+// للاستدعاء use عند استخدام كلاس معين
+/*
+في php يسمح يكون عندي اكتر من كلاس بنفس السم ولكن بمسار مختلف
+*/
+/*
+لما يكون الرابط مس موجود
+
+دالة fallback لحتى اغير صفحة404
+
+App\Providers\RouteServiceProvider بروح اغير رابط الويب للرابط يلي بدي ياه
+ضغط مرتين و alt
+
+*/
 
 /*
+
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
@@ -14,9 +34,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "welcome";
 });
 
 Route::get('/about', function () {
     return view('about');
 });
+
+/*
+اسرع منput من patch (تعديل )
+عنصر واحد فقط put من patch
+*/
+// about , contact , services , hala
+// .$اسم المتغير في ال return
+
+
+
